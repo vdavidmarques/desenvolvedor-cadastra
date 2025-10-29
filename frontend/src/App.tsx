72f9b8aa806e1ebeadda5e3de94b9144becec3ed
vdavidmarques <vdavidmarques@gmail.com>
2025-10-29 00:13:29 -0300

    feat: header, footer, logo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import { ProductPage } from './components/ProductPage/ProductPage';
+import './styles/main.scss';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <ProductPage />
+      <Footer />
+    </>
+  );
+}
+
+export default App
---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <h4 className='footer--title'>CADASTRA: Implantação de E-commerce VTEX</h4>
+    </footer>
+  )
+}
+
+export default Footer
